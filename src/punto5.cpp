82396480c0c54b9101140ce9a5aac98bddc4d8cd
Laumaro08 <Laumaro200@gmail.com>
2025-04-09 16:52:01 -0300

    a

---
+++ src/punto5.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+uint16_t Ent, resta;
+void setup() {
+RCC->APB2ENR|=RCC_APB2ENR_IOPAEN_Msk;
+RCC->APB2ENR|=RCC_APB2ENR_IOPBEN_Msk;
+GPIOA->CRL=0X44444444;
+GPIOA->CRH=0X44444444;
+GPIOB->CRL=0X11111111;
+GPIOB->CRH=0X11111111;
+}
+void loop() {
+  Ent=GPIOB->IDR;
+  resta= Ent-0x23;
+  GPIOA->BSRR=Ent;
+
+
+}
